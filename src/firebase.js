3beea3c02199f553c80e4ced7b6dd7b93cc17f8e
Design-n-Techie <designntechie@gmail.com>
2023-11-21 19:24:49 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,6 +3,13 @@ import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   //config
+  apiKey: "AIzaSyCh1sgwQoxXj0FjBLmAVeW5veYlmVJeWRM",
+  authDomain: "login-react-3a2b6.firebaseapp.com",
+  projectId: "login-react-3a2b6",
+  storageBucket: "login-react-3a2b6.appspot.com",
+  messagingSenderId: "1073593915712",
+  appId: "1:1073593915712:web:aae8a97ce753518013c2a5",
+  measurementId: "G-WCT1YJ6LX5"
 };
 
 const app = initializeApp(firebaseConfig);
